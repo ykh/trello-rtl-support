4190d50bc38451aa34e16b43d364882a9366b060
Yaser Khahani <yaserkh@gmail.com>
2014-12-15 09:32:56 +0330

    Apply closure and update doc file

--- script.js
+++ script.js
@@ -1,64 +1,67 @@
-/**
- * Developer: Yaser Khahani
- * URL: https://github.com/ykh/trello-rtl-support
- * 2014
- **/ 
-var doms = 'h1, h2, h3, p ,a',
-    inputs = 'textarea, input[type=text]';
+/*
+ * trello-rtl-support
+ * https://github.com/ykh/trello-rtl-support
+ * This script is a solution to trello can detect rtl content and apply correct behaviors
+ * Licensed under the GNU GPL v2 license.
+ */
+(function () {
+    var doms = 'h1, h2, h3, p ,a',
+        inputs = 'textarea, input[type=text]';
 
-$(document).ajaxComplete(function () {
-    $(doms).each(function () {
+    $(document).ajaxComplete(function () {
+        $(doms).each(function () {
+            updateStyle($(this));
+        });
+    });
+
+    $(document).ready(function () {
+        $(doms).each(function () {
+            updateStyle($(this));
+        });
+    });
+
+    $('body').on('input blur focus', inputs, function (e) {
         updateStyle($(this));
     });
-});
 
-$(document).ready(function () {
-    $(doms).each(function () {
-        updateStyle($(this));
-    });
-});
+    function updateStyle(target) {
+        var regex,
+            matched,
+            dir,
+            align,
+            value = '',
+            tagName = target[0].tagName,
+            rtl,
+            ltr;
 
-$('body').on('input blur focus', inputs, function (e) {
-    updateStyle($(this));
-});
+        rtl = {
+            'direction': 'rtl',
+            'text-align': 'right'
+        };
+        ltr = {
+            'direction': 'ltr',
+            'text-align': 'left'
+        };
 
-function updateStyle(target) {
-    var regex,
-        matched,
-        dir,
-        align,
-        value = '',
-        tagName = target[0].tagName,
-        rtl,
-        ltr;
+        value = target.text();
 
-    rtl = {
-        'direction': 'rtl',
-        'text-align': 'right'        
-    };   
-    ltr = {
-        'direction': 'ltr',
-        'text-align': 'left'        
-    };
-        
-    value = target.text();
-    
-    if (target.is('textarea') || target.is('input[type=text]')) {
-        value = target.val();
+        if (target.is('textarea') || target.is('input[type=text]')) {
+            value = target.val();
+        }
+
+        if (target.is('a') || tagName === 'H1' || tagName === 'H2' || tagName === 'H3') {
+            rtl['unicode-bidi'] = 'embed';
+            ltr['unicode-bidi'] = '';
+        }
+
+        regex = /[\u0600-\u06FF]/;
+
+        matched = value.match(regex);
+
+        if (matched) {
+            target.css(rtl);
+        } else {
+            target.css(ltr);
+        }
     }
-    
-    if (target.is('a') || tagName === 'H1' || tagName === 'H2' || tagName === 'H3') {
-        rtl['unicode-bidi'] = 'embed';
-        ltr['unicode-bidi'] = '';
-    }
-        
-    regex = /[\u0600-\u06FF]/;
-    
-    matched = value.match(regex);
-    
-    if (matched) {
-            target.css(rtl);            
-    } else {
-        target.css(ltr);        
-    }
-}
+})();
